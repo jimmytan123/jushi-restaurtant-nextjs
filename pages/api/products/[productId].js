2bae63a5599b12c28934b5da6c12aaaf04cc57a6
Jimmy Tan <jimmytan0424@gmail.com>
2022-02-03 20:54:42 -0800

    add err handler for backend

--- pages/api/products/[productId].js
+++ pages/api/products/[productId].js
@@ -1,20 +1,26 @@
 import { MongoClient, ObjectId } from 'mongodb';
 
+// .../api/products/[productId]
+
 async function handler(req, res) {
   const client = await MongoClient.connect(process.env.MONGO_URL);
   const db = client.db();
   const productsCollection = db.collection('products');
 
   if (req.method === 'GET') {
-    // find object by id
-    const selectedProduct = await productsCollection.findOne({
-      _id: ObjectId(req.query.productId), // convert productId into ObjectId object -- same as the id form in the MongoDB, so it can search
-    });
+    try {
+      // find object by id
+      const selectedProduct = await productsCollection.findOne({
+        _id: ObjectId(req.query.productId), // convert productId into ObjectId object -- same as the id form in the MongoDB, so it can search
+      });
 
-    client.close();
+      client.close();
 
-    // send back response
-    res.status(200).json(selectedProduct);
+      // send back response
+      res.status(200).json(selectedProduct);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   }
 }
 
--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,16 +1,22 @@
 import { MongoClient } from 'mongodb';
 
+// .../api/products
+
 async function handler(req, res) {
   if (req.method === 'GET') {
-    const client = await MongoClient.connect(process.env.MONGO_URL);
-    const db = client.db();
-    const productsCollection = db.collection('products');
-    const result = productsCollection.find();
-    const products = await result.toArray();
+    try {
+      const client = await MongoClient.connect(process.env.MONGO_URL);
+      const db = client.db();
+      const productsCollection = db.collection('products');
+      const result = productsCollection.find();
+      const products = await result.toArray();
 
-    await client.close();
-    // send back response
-    res.status(200).json(products);
+      await client.close();
+      // send back response
+      res.status(200).json(products);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   }
 }
 
